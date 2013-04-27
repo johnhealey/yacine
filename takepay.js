dc99c7a12528d9f98d18c843e785bcbd4fac4350
johnhealey <johnhealey05@gmail.com>
2013-04-27 23:57:11 +0800

    test

--- takepay.js
+++ takepay.js
@@ -67,7 +67,7 @@
             });
         };
 	    function _setupForm(){
-
+	        _div.style.marginLeft='120px';
 	        
 	        var _form = document.createElement("div");
 	        _div.id="outside";
